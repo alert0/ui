2ff120531ab6acac13e24153fa80452b02862dd7
anginfo <1348898564@qq.com>
2017-04-12 11:21:01 +0800

    按照大屏设计样式布局

--- js/main.js
+++ js/main.js
@@ -3,42 +3,32 @@
  */
 
 $(function () {
-	
-    // 切换地图球形和平面的显示
-    var txt = $("#code-source").text();
-    var displaySphere;//当前状态
 
-    if (localStorage.mapDisplay === "sphere") {
-        txt = txt.replace("flat: false", "flat: true");
-        displaySphere = true;
-    } else {
-        txt = txt.replace("flat: true", "flat: false");
-        displaySphere = false;
-    }
-    $("#code-source").text(txt);
+    //切换echarts-x和chearts的显示
+    var $main = $("#main");
+    var sphere = '<div id="chart" ></div>';
+    var iframe = '<iframe id="iframe" scrolling="no" frameborder="0" src="flatMap.html" ></iframe>';
+    var $btn = $("#left").find(".btnGroup img").click(function () {
+        var src = $(this).attr("src");
+        if (src.indexOf("off") >= 0) {
+            src = src.replace("off", "on");
+            $(this).attr("src", src)
+                .siblings().attr("src", $(this).siblings().attr("src").replace("on", "off"))
+            var $chart = $("#chart");
+            if ($chart.length > 0) {
+                $chart.remove();
+                $main.append(iframe)
+            } else {
+                $("#iframe").remove();
+                $main.append(sphere);
+                require(['start'], function (start) {
+                    setTimeout(function () {
+                        start.runCode()
+                    });
 
-    $("#switch").click(function () {/*点击事件*/
-        if(displaySphere){/*localStorage不能直接存储布尔值！！！*/
-            localStorage.mapDisplay = "flat";
-        }else{
-            localStorage.mapDisplay = "sphere";
+                });
+            }
         }
-        document.location.reload();
-    });
-
-
-    require(['start'],function (start) {
-        // alert(start.test(5));
-        $("#bigger").click(function () {
-            start.zoom1(0,0,0,100,100)
-        })
-        $("#switch").click(function () {
-            // start.showFlat()
-        })
-        $("#echarts2").click(function () {
-
-        })
-    });
-
+    })
 
 })
---
+++ js/sphere.js
@@ -0,0 +1,140 @@
+var mapSphere = function mapSphere() {
+    myChart.showLoading();
+        $.ajax({
+            url: '../data/hailianTest01.json',
+            success: function (data) {
+                var markPointStyle = {
+                    normal: {
+                        color: 'red'
+                    }
+                };
+                // Airport: [name（机场名？）, city, country, longitude（经度）, latitude（纬度）]
+                //map和forEach都可以对数组进行遍历，区别是map可以返回一个修改后的数组，但不影响原数组
+                var airports = data.airports.map(function (item) {
+                    //返回带有标记样式和经纬度的对象
+                    return {
+                        itemStyle: markPointStyle,
+                        geoCoord: [item[3], item[4]]
+                    }
+                });
+
+                // Route: [airlineIndex(航空公司编号), sourceAirportIndex（起点）, destinationAirportIndex（终点）]
+                var routesGroupByAirline = {};
+
+                data.routes.forEach(function (route) {//遍历所有航线
+
+                    //airlines是所有图例（航空公司），其每个下标就是航空公司编号（route[0]）
+                    var airline = data.airlines[route[0]];
+                    var airlineName = airline[0];
+
+                    //对象的属性名是变量时需要用中括号来获取值
+                    if (!routesGroupByAirline[airlineName]) {
+                        routesGroupByAirline[airlineName] = [];
+                    }
+                    routesGroupByAirline[airlineName].push(route);
+                });
+
+                var opts = {
+                    legend: {
+                        show: true,
+                        //   遍历航空公司名称显示图例
+                        data: data.airlines.map(function (item, index, array) {
+                            return item[0];
+                        }),
+                        selected: {},
+                        x: 'left',
+                        orient: 'vertical',
+                        textStyle: {
+                            color: 'white'
+                        }
+                    },
+                    tooltip: {
+                        formatter: '{b}'
+                    },
+                    series: [{
+                        type: 'map3d',
+                        mapType: 'world',
+                        flat: false, /*是否使用平面图*/
+                        flatAngle: 0,
+                        mapLocation: {
+                            x: "22%",
+                            y: "top"
+                        },
+                        baseLayer: {
+                            backgroundColor: '',
+                            backgroundImage: '../img/earth.jpg'
+                        },
+                        itemStyle: {
+                            normal: {
+                                borderWidth: 1,
+                                borderColor: 'yellow',
+                                areaStyle: {
+                                    color: 'rgba(0, 0, 0, 0)'
+                                }
+                            }
+                        },
+                        markPoint: {
+                            symbol: "heart",
+                            effect: {
+                                shadowBlur: 0.2
+                            },
+                            large: true,
+                            symbolSize: 15,
+                            data: airports
+                        },
+                        roam: {
+                            autoRotateAfterStill: 30,
+                            focus: 'China'
+                        }
+                    }]
+                };
+
+                //默认选中华北
+                opts.legend.data.forEach(function (name) {
+                    if (name.indexOf('华北') >= 0) {
+                        opts.legend.selected[name] = true;
+                    } else {
+                        opts.legend.selected[name] = false;
+                    }
+                });
+                /*
+                 "青岛":[120.33,36.07],
+                 "北京":[116.46,39.92],
+                 */
+                data.airlines.forEach(function (item) {
+                    var airlineName = item[0];
+                    var routes = routesGroupByAirline[airlineName];
+                    if (routes) {
+                        opts.series.push({
+                            type: 'map3d',
+                            name: airlineName,
+                            markLine: {
+                                effect: {
+                                    show: true
+                                },
+                                distance: 2,
+                                data: routes.map(function (item) {
+                                    //geoCoord：通过绝对经纬度指定地区的名称文本位置
+                                    //route中的起点终点数值是机场的下标！
+                                    return [{
+                                        // 获取起点的经纬度
+                                        geoCoord: airports[item[1]].geoCoord
+                                    }, {
+                                        // 获取终点的经纬度
+                                        geoCoord: airports[item[2]].geoCoord
+                                    }]
+                                })
+                            }
+                        });
+                    }
+                });
+
+                myChart.setOption(opts);
+
+                myChart.hideLoading();
+
+            }
+
+        });
+
+};
